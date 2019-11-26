3f0b523eab6174c5dda052c62378964fe1f4a52e
Jose David Caro <j.caro0207@gmail.com>
2019-11-26 21:41:46 +0000

    [Adding Header file to Jose] Adding header file of the program

---
+++ header.h
@@ -0,0 +1,23 @@
+#ifndef HEADER_H
+#define HEADER_H
+
+#include <stdio.h>
+#include <stdlib.h>
+#include <string.h>
+#include <unistd.h>
+#include <fcntl.h>
+#include <sys/types.h>
+#include <sys/stat.h>
+#include <sys/wait.h>
+#include <signal.h>
+#include <unistd.h>
+int _putchar(char c);
+void print_number(int n);
+int _strlen_recursion(char *s);
+int _atoi(char *s);
+
+
+
+
+
+#endif /* HEADER_H */
